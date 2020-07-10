23da8ddb6861950704cda754df8eff9baf84fdca
Oliver <oliver@eastpoint.co.uk>
2020-07-10 10:42:04 +0100

    Introduced sample page

--- app/src/main/java/uk/co/eastpoint/test/nordicdfu/dfuactivity.kt
+++ app/src/main/java/uk/co/eastpoint/test/nordicdfu/dfuactivity.kt
@@ -12,9 +12,7 @@ class dfuactivity : AppCompatActivity() {
         setContentView(R.layout.activity_dfuactivity)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+
         }
     }
 }
